0a349e114169213bd8a83186b5af07efb748359a
Skratymir <97696626+Skratymir@users.noreply.github.com>
2022-05-05 13:03:47 +0200

    Add login feature

---
+++ launcher.py
@@ -0,0 +1,44 @@
+import minecraft_launcher_lib
+import sys
+import pickle
+
+class Launcher():
+    def __init__(self):
+        self.CLIENT_ID = "9ff1e48d-5b3c-42bb-883f-fd0426a583c4"
+        self.SECRET = "7Hw8Q~1pfZRlrL2Pfo9QEF~cZahZw5pxhRDv0b0G"
+        self.REDIRECT_URL = "http://localhost/"
+        self.minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
+        self.login_data = None
+        self.logged_in = False
+        
+    def login(self):
+        if self.check_login == False:
+            print(f"Open {minecraft_launcher_lib.microsoft_account.get_login_url(self.CLIENT_ID, self.REDIRECT_URL) } in your Browser, login and paste the url you are redirected to here: ")
+            code_url = input()
+            
+            if not minecraft_launcher_lib.microsoft_account.url_contains_auth_code(code_url):
+                print("Url invalid")
+                sys.exit(1)
+                
+            auth_code = minecraft_launcher_lib.microsoft_account.get_auth_code_from_url(code_url)
+            login_data = minecraft_launcher_lib.microsoft_account.complete_login(self.CLIENT_ID, self.SECRET, self.REDIRECT_URL, auth_code)
+            with open("login.pkl", "wb") as f:
+                pickle.dump(login_data, f)
+            print("Login sucessful")
+            self.logged_in = True
+    
+    def refresh_login(self):
+        try:
+            with open("login.pkl", "rb") as f:
+                login_data = pickle.load(f)
+            minecraft_launcher_lib.microsoft_account.complete_refresh(self.CLIENT_ID, self.SECRET, self.REDIRECT_URL, login_data["refresh_token"])
+            print("Logged in as {}".format(login_data["name"]))
+            self.logged_in = True
+        except FileNotFoundError:
+            print("No login data detected. Please login from the launcher")
+            
+    def check_login(self):
+        if self.logged_in:
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tkinter
+import launcher
+from threading import Thread
 
 class Window(tkinter.Tk):
-    def __init__(self, *args, **kwargs):
-        tkinter.Tk.__init__(self, *args, **kwargs)
+    def __init__(self, slanch):
+        tkinter.Tk.__init__(self)
         self.title("Slanch")
         self.geometry(
             str(round(self.winfo_screenwidth() / 3)) + 
@@ -22,7 +24,12 @@ class Window(tkinter.Tk):
         self.frames = {}
         
         self.frames["LaunchPage"] = LaunchPage(parent=container, controller=self)
+        self.frames["ProfilesPage"] = ProfilesPage(parent=container, controller=self)
+        self.frames["SettingsPage"] = SettingsPage(parent=container, controller=self, slanch=slanch)
+        
         self.frames["LaunchPage"].grid(row=0, column=0, sticky="nsew")
+        self.frames["ProfilesPage"].grid(row=0, column=0, sticky="nsew")
+        self.frames["SettingsPage"].grid(row=0, column=0, sticky="nsew")
         
         self.set_page("LaunchPage")
         
@@ -34,11 +41,52 @@ class LaunchPage(tkinter.Frame):
     def __init__(self, parent, controller):
         tkinter.Frame.__init__(self, parent)
         self.controller = controller
-        self.setup()
         
-    def setup(self):
+        self.launch_page_button = tkinter.Button(self)
+        self.profiles_page_button = tkinter.Button(self, command=lambda: controller.set_page("ProfilesPage"))
+        self.settings_page_button = tkinter.Button(self, command=lambda: controller.set_page("SettingsPage"))
+        
+        self.launch_page_button.place(relx=0.165, rely=0, relwidth=0.33, relheight=0.15, anchor="n")
+        self.profiles_page_button.place(relx=0.5, rely=0, relwidth=0.33, relheight=0.15, anchor="n")
+        self.settings_page_button.place(relx=0.83, rely=0, relwidth=0.33, relheight=0.15, anchor="n")
+        
         self.launch_button = tkinter.Button(self, text="Launch")
+        
         self.launch_button.place(relx=0.5, rely=0.5, anchor="center", relwidth=0.3, relheight=0.3)
+        
+class ProfilesPage(tkinter.Frame):
+    def __init__(self, parent, controller):
+        tkinter.Frame.__init__(self, parent)
+        self.controller = controller
+        
+        self.launch_page_button = tkinter.Button(self, command=lambda: controller.set_page("LaunchPage"))
+        self.profiles_page_button = tkinter.Button(self)
+        self.settings_page_button = tkinter.Button(self, command=lambda: controller.set_page("SettingsPage"))
+        
+        self.launch_page_button.place(relx=0.165, rely=0, relwidth=0.33, relheight=0.15, anchor="n")
+        self.profiles_page_button.place(relx=0.5, rely=0, relwidth=0.33, relheight=0.15, anchor="n")
+        self.settings_page_button.place(relx=0.83, rely=0, relwidth=0.33, relheight=0.15, anchor="n")
+        
+class SettingsPage(tkinter.Frame):
+    def __init__(self, parent, controller, slanch):
+        tkinter.Frame.__init__(self, parent)
+        self.controller = controller
+        self.slanch = launcher.Launcher
+        
+        self.launch_page_button = tkinter.Button(self, command=lambda: controller.set_page("LaunchPage"))
+        self.profiles_page_button = tkinter.Button(self, command=lambda: controller.set_page("ProfilesPage"))
+        self.settings_page_button = tkinter.Button(self)
+        
+        self.launch_page_button.place(relx=0.165, rely=0, relwidth=0.33, relheight=0.15, anchor="n")
+        self.profiles_page_button.place(relx=0.5, rely=0, relwidth=0.33, relheight=0.15, anchor="n")
+        self.settings_page_button.place(relx=0.83, rely=0, relwidth=0.33, relheight=0.15, anchor="n")
+        
+        self.login_button = tkinter.Button(self, text="Login", command=lambda: slanch.login())
+        
+        self.login_button.place(relx=0.5, rely=0.2, relwidth=0.3, relheight=0.15, anchor="n")
 
-window = Window()
-window.mainloop()
+if __name__ == "__main__":
+    slanch = launcher.Launcher()
+    window = Window(slanch)
+    Thread(target=slanch.refresh_login).start()
+    window.mainloop()
